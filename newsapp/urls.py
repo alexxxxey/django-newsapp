82a8aedfd2a464c3a3e3471f05aa20d4272505d7
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2015-04-19 14:56:06 +0300

    Fix render one new if archive enabled

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('newsapp.views',
     url(r'^$', 'news_list', name="news_all"),
     url(r'^page-1/$', lambda x: redirect('news_all', permanent=True)),
     url(r'^page-(?P<page>[\d]+)/$', 'news_list'),
-    url(r'^(?P<opened_url>[\d\w-]+)/$', 'render_new'),
 )
 
 if ENABLE_ARCHIVE:
@@ -17,4 +16,6 @@ if ENABLE_ARCHIVE:
         url(r'^(?P<year>\d{4})/(?P<month>[1-9]|10|11|12)/page-(?P<page>[\d]+)/$', 'news_list'),
     )
 
-
+urlpatterns += patterns('newsapp.views',
+    url(r'^(?P<opened_url>[\d\w-]+)/$', 'render_new'),
+)
