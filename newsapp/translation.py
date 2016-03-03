d4c5e04acc03b57e581ae1ec6d34454c3f326c0e
Alexey <a@trialine.lv>
2016-03-03 15:05:55 +0200

    fix auto modeltranslation

--- newsapp/translation.py
+++ newsapp/translation.py
@@ -1,13 +1,27 @@
-# from modeltranslation.translator import translator, TranslationOptions, AlreadyRegistered
-# from .models import New
+from modeltranslation.translator import translator, TranslationOptions, AlreadyRegistered
+from .models import New
+from settings_newsapp import ENABLE_CATEGORIES
 
-# for model in [New]:
-#     if hasattr(model, '_translation_fields'):
-#         translation_option = type("{0}Translation".format(model.__name__), (TranslationOptions,), {
-#             'fields': model._translation_fields,
-#         })
-#         try:
-#             translator.register(model, translation_option)
-#         except AlreadyRegistered:
-#             pass
+for model in [New,]:
+    if hasattr(model, '_translation_fields'):
+        translation_option = type("{0}Translation".format(model.__name__), (TranslationOptions,), {
+            'fields': model._translation_fields,
+        })
+        try:
+            translator.register(model, translation_option)
+        except AlreadyRegistered:
+            pass
 
+
+if ENABLE_CATEGORIES:
+	from .models import NewCategory
+
+	for model in [NewCategory,]:
+	    if hasattr(model, '_translation_fields'):
+	        translation_option = type("{0}Translation".format(model.__name__), (TranslationOptions,), {
+	            'fields': model._translation_fields,
+	        })
+	        try:
+	            translator.register(model, translation_option)
+	        except AlreadyRegistered:
+	            pass
