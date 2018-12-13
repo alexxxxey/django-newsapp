6f793fe30a64c314bccc014aacfbd75dfda529d0
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2018-12-13 23:59:08 +0200

    Bump up version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import os
 from setuptools import setup
 
 setup(name='django-newsapp',
-      version='0.5.1',
+      version='0.5.2',
       description='Django news module',
       long_description='Django news module',
       author='Alexey Osovitniy',
       author_email='a@trialine.lv',
-      packages=['newsapp',],
+      packages=['newsapp'],
       url='https://github.com/alexxxxey/django-news-app',
       include_package_data=True,
       zip_safe=False,
