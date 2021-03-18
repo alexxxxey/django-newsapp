39f5be5b66e311f3d128ab0e1f5a4763a84f0987
Sergej <sergejs@trialine.lv>
2021-03-18 18:58:25 +0200

    tags url fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
       name='django-newsapp',
-      version='1.0.0',
+      version='1.0.1',
       packages=find_packages(),
       include_package_data=True,
       description='Django News/Blog module',
